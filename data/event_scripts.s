296559ef803ed907f8bb7adc06d5a9af0cb8503b
shamaneast <mike.hoffmeister@gmail.com>
2026-04-14 14:40:46 +0200

    Messing around with pallet town maps and connection

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1736,3 +1736,6 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+
+	.include "data/maps/Route1_Frlg_2/scripts.inc"
+	.include "data/maps/Route1_Frlg_2/text.inc"
