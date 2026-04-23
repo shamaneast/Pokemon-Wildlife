68220eb89728aad7fe820a301dfcec28b32fc07b
shamaneast <148709868+shamaneast@users.noreply.github.com>
2026-04-23 12:45:31 +0200

    Add visible overworld wild Pokémon spawning system

---
+++ include/overworld_wild.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_OVERWORLD_WILD_H
+#define GUARD_OVERWORLD_WILD_H
+
+#include "global.h"
+
+void OverworldWild_ResetState(void);
+bool32 OverworldWild_OnStep(void);
+bool8 OverworldWild_UseVisibleEncounters(void);
+
+#endif // GUARD_OVERWORLD_WILD_H
